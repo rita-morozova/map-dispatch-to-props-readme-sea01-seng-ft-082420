690d993407d28ad6f485da60202e77a9ec83817b
Margarita Morozova <ritamorozova@yahoo.com>
2020-11-15 14:02:42 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.addItem()
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={(event) => this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -27,4 +27,5 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+
+export default connect(mapStateToProps, { addItem })(App)
